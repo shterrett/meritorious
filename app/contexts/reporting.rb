e0e652bc458d051ad022ad1abe3613d808de1f3c
Stuart Terrett <shterrett@gmail.com>
2014-02-14 06:29:12 -0500

    Assign marks to students in meeting

--- app/contexts/reporting.rb
+++ app/contexts/reporting.rb
@@ -3,18 +3,36 @@ class Reporting
     @reporter = reporter.as(Reporter)
   end
 
-  def csv_data(type)
+  def csv_data(type, *args)
     CsvReportPresenter.new(['Student Name', 'Student id', 'Number of Merits', 'Number of Demerits'],
-                           send("by_#{type}").map(&:to_row)
+                           data_for_csv(type, *args).map(&:to_row)
                           )
+  end
+
+  def data(type, *args)
+    send("by_#{type}", *args)
+  end
+
+  private
+
+  def data_for_csv(type, *args)
+    [data(type, *args)].flatten
   end
 
   def by_meeting
     @reporter.classroom.students.each_with_object([]) do |student, students|
-      students << StudentMarksPresenter.new(student).tap do |presenter|
-        presenter.add_merits @reporter.merits.where(student_id: student.id)
-        presenter.add_demerits @reporter.demerits.where(student_id: student.id)
-      end
+      students << student_presenter_for(student)
+    end
+  end
+
+  def by_student_meeting(student)
+    student_presenter_for(student)
+  end
+
+  def student_presenter_for(student)
+    StudentMarksPresenter.new(student).tap do |presenter|
+      presenter.add_merits @reporter.merits.where(student_id: student.id)
+      presenter.add_demerits @reporter.demerits.where(student_id: student.id)
     end
   end
 end
--- spec/presenters/student_marks_presenter_spec.rb
+++ spec/presenters/student_marks_presenter_spec.rb
@@ -3,19 +3,28 @@ require 'spec_helper'
 describe StudentMarksPresenter do
   describe 'student methods' do
     it 'returns the student name' do
-      student = create(:student)
+      student = build_stubbed(:student)
       presenter = StudentMarksPresenter.new(student)
 
       expect(presenter.name).to eq(student.name)
     end
 
     it 'returns the student_id' do
-      student = create(:student)
+      student = build_stubbed(:student)
       presenter = StudentMarksPresenter.new(student)
 
       expect(presenter.student_id).to eq(student.student_id)
     end
 
+    it 'returns the id' do
+      student = build_stubbed(:student)
+      presenter = StudentMarksPresenter.new(student)
+
+      expect(presenter.id).to eq(student.id)
+    end
+  end
+
+  describe 'marks methods' do
     it 'can add a list of merits to existing merits' do
       merit = create(:mark, content_type: 'Merit', content: create(:merit))
       student = create(:student)
@@ -34,6 +43,26 @@ describe StudentMarksPresenter do
       expect(presenter.demerits).to match_array([demerit])
     end
 
+    it 'returns the count of merits' do
+      merit = create(:mark, content_type: 'Merit', content: create(:merit))
+      student = create(:student)
+      presenter = StudentMarksPresenter.new(student)
+      presenter.add_merits([merit])
+
+      expect(presenter.merits_count).to eq(1)
+    end
+
+    it 'returns the count of demerits' do
+      demerit = create(:mark, content_type: 'Merit', content: create(:demerit))
+      student = create(:student)
+      presenter = StudentMarksPresenter.new(student)
+      presenter.add_demerits([demerit])
+
+      expect(presenter.demerits_count).to eq(1)
+    end
+  end
+
+  describe 'formatting' do
     it 'formats the information in a csv row' do
       merit = create(:mark, content_type: 'Merit', content: create(:merit))
       demerit = create(:mark, content_type: 'Demerit', content: create(:demerit))
